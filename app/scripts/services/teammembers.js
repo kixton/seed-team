a441e9fa1d9118b7babd206373def9cb2122f411
Kim Pham <ms.kimpham@gmail.com>
2015-04-27 15:42:11 -0700

    Minor css edits

--- app/scripts/services/teammembers.js
+++ app/scripts/services/teammembers.js
@@ -19,16 +19,16 @@ angular.module('seedApp')
       },
       {
       name: 'Ryan Hildebrand',
-      title: 'CTO and Co-Founder',
-      summary: 'Ryan was previously VP of Finance at Simple, an online banking company that was acquired by BBVA. He has held leadership roles at Demandforce and Strands. Ryan is a former  CPA with a BS in Accounting and International Business from Oregon State University.',
-      twitter: '',
+      title: 'CFO and Co-Founder',
+      summary: "Ryan was previously VP of Finance at Simple, an online banking company that was acquired by BBVA. He has held leadership roles at Demandforce and Strands. Ryan is a former  CPA with a BS in Accounting and International Business from Oregon State University.",
+      twitter: 'hollywoodcpa',
       linkedin: 'mrryanhildebrand',
       imgUrl: 'https://d1qb2nb5cznatu.cloudfront.net/users/9533-large_jpg'
       },
       {
       name: 'Christopher Swenson',
       title: 'Principal Engineer',
-      summary: 'Christopher was previously an engineer at Google and Simple. He has also consulted numerous clients on secruity and cryptography. He has published a book on crytography, Modern Cryptanalysis. Christopher has a PhD in Computer Science from University of Tulsa.',
+      summary: 'Christopher was previously an engineer at Google and Simple. He previously owned his own consultancy and has consulted numerous clients on secruity. He has published a book on crytography, Modern Cryptanalysis. Christopher has a PhD in Computer Science from University of Tulsa.',
       twitter: 'chris_swenson',
       linkedin: 'christopherswenson',
       imgUrl: 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/1/000/263/25e/04872e5.jpg'
